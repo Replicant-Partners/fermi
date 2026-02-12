32fddc29b603354a11bab13eda40e51698dd617f
labra.studio <i@labra.studio>
2026-02-12 16:44:38 +0100

    Add invite/share flow: contacts screen invite friends, rabble chat invite contacts, host rabble contact picker for private rabbles, home_shell invite button
    - Contacts screen: 'Invite friends' and 'New rabble' quick action buttons, copy invite link bottom sheet
    - Rabble chat: person_add icon in app bar opens invite sheet with contact picker + share link
    - Host rabble: Friends Only visibility shows contact picker with checkboxes, auto-invites on create
    - Home shell: person_add icon in app bar + 'Invite friends' in account menu
    - All sharing via clipboard (copy invite link to paste in WhatsApp/etc)

--- static/rabble/flutter_bootstrap.js
+++ static/rabble/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"82bd5b7209295a5b7ff8cae0df96e7870171e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2876274112"
+    serviceWorkerVersion: "2037239496"
   }
 });
