8eea55648045e2cfa7527d11b79d1323c8377163
labra.studio <i@labra.studio>
2026-02-16 04:09:03 +0100

    Vibrant activity feed: vivid species colors, game-like action chips, sparkle star affordance, expedition drill-down teasers

--- rabble-web/flutter_bootstrap.js
+++ rabble-web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"82bd5b7209295a5b7ff8cae0df96e7870171e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "774600996"
+    serviceWorkerVersion: "1823246569"
   }
 });
